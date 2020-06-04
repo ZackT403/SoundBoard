bd4254d22dfaaf3ce2954f02259adfdf2eeb7b1c
zackproject <zackdavidturner@gmail.com>
2020-06-04 11:37:11 +0100

    home page added

--- sound/views.py
+++ sound/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Main(View):
+    @staticmethod
+    def get(request):
+        return render(request,'sound/main.html')
+
+    @staticmethod
+    def post(request):
+        pass
--- sound_board/urls.py
+++ sound_board/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from sound.views import Main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Main.as_view())
 ]
 
 if settings.DEBUG:
