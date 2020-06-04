331ede6d350df76b9d3bf57a040a86715ca2f966
zackproject <zackdavidturner@gmail.com>
2020-06-04 11:13:50 +0100

    new file commit

--- sound_board/settings.py
+++ sound_board/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static'),
 )
 MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = (
-    os.path.join(BASE_DIR,'media')
-)
+MEDIA_ROOT = (os.path.join(BASE_DIR,'media'))
