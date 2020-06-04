1e73f74d95bb8bdeeb6113c910b70194f9494b7c
zackproject <zackdavidturner@gmail.com>
2020-06-04 16:15:44 +0100

    html updates with javascript

--- sound/models.py
+++ sound/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class SoundPost(models.Model):
     sound = models.FileField(upload_to='sounds')
-    image = models.ImageField(upload_to='images', default='default.png')
+    image = models.ImageField(upload_to='images', default='images/default.png')
     title = models.CharField(max_length=100, default='sound file')
 
     def __str__(self):
