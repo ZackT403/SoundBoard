964bb9d526073b22585feeac52194570a9596f41
zackproject <zackdavidturner@gmail.com>
2020-06-05 13:14:30 +0100

    file type validation check

--- sound/views.py
+++ sound/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import SoundPost
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class MainView(ListView):
     template_name = 'sound/main.html'
@@ -11,12 +11,16 @@ class MainView(ListView):
         return SoundPost.objects.all().order_by('?')
 
 
-class CreatePost(LoginRequiredMixin,CreateView):
+class CreatePost(LoginRequiredMixin, CreateView):
     model = SoundPost
     success_url = '/'
     fields = ['title', 'sound', 'image']
     template_name = 'sound/postsound.html'
 
     def form_valid(self, form):
+        audio = str(form.cleaned_data['sound'])
+        audio_type = audio.split('.')[-1:][0]
+        if 'mp3' not in audio_type:
+            print('not mp3')
         form.instance.author = self.request.user
         return super().form_valid(form)
