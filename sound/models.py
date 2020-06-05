f474c2af7391f1e661654442f9ed53ad28ca1875
zackproject <zackdavidturner@gmail.com>
2020-06-05 12:08:47 +0100

    added perms and some updates

--- sound/models.py
+++ sound/models.py
@@ -1,11 +1,21 @@
 from django.db import models
-
+from PIL import Image
+from django.contrib.auth.models import User
 
 # Create your models here.
 class SoundPost(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,default='')
     sound = models.FileField(upload_to='sounds')
     image = models.ImageField(upload_to='images', default='images/default.png')
     title = models.CharField(max_length=100)
+
+    def save(self, **kwargs):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 100 or img.width > 100:
+            img.thumbnail((100, 100))
+            img.save(self.image.path)
 
     def __str__(self):
         return self.title
--- sound/views.py
+++ sound/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.views import View
-from django.views import generic
+from django.views.generic import ListView, CreateView
 from .models import SoundPost
-from django.shortcuts import HttpResponseRedirect
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class MainView(generic.ListView):
+class MainView(ListView):
     template_name = 'sound/main.html'
     context_object_name = 'sound'
 
@@ -13,10 +11,12 @@ class MainView(generic.ListView):
         return SoundPost.objects.all().order_by('?')
 
 
-class CreatePost(CreateView):
+class CreatePost(LoginRequiredMixin,CreateView):
     model = SoundPost
     success_url = '/'
-    fields = ['title','sound','image']
+    fields = ['title', 'sound', 'image']
     template_name = 'sound/postsound.html'
-    def  form_valid(self, form):
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
         return super().form_valid(form)
