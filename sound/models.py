72e3ed13e6eea846e48cb75b2ca8aca574c87683
ZackT123 <zackdavidturner@gmail.com>
2020-06-11 18:23:12 +0000

    Latest with user posts

--- sound/models.py
+++ sound/models.py
@@ -8,7 +8,7 @@ class SoundPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     sound = models.FileField(upload_to='sounds')
     image = models.ImageField(upload_to='images', default='images/default.png')
-    title = models.CharField(max_length=23)
+    title = models.CharField(max_length=20)
 
     def save(self, **kwargs):
         super().save()
